e018f975b26a5c2e08c5096146e3777d0f80f20a
Euclidr <euclidry@gmail.com>
2019-09-29 16:07:21 +0800

    bench skipset

--- benches/skipset_benchmark.rs
+++ benches/skipset_benchmark.rs
@@ -1,65 +1,113 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
+use criterion::{Criterion, BenchmarkId};
 use skiplist::skipset::SkipSet;
 use std::collections::{BTreeSet, HashSet};
 
-fn skipset_cardinal_close_benchmark(c: &mut Criterion) {
-    let mut sets1 = SkipSet::new();
-    let mut sets2 = SkipSet::new();
-    for i in 0..500 {
-        sets1.add(i * 2);
-        sets2.add(i * 3);
+
+// use std::iter;
+
+// use criterion::Throughput;
+
+// fn from_elem(c: &mut Criterion) {
+//     static KB: usize = 1024;
+
+//     let mut group = c.benchmark_group("from_elem");
+//     for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter() {
+//         group.throughput(Throughput::Bytes(*size as u64));
+//         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+//             b.iter(|| iter::repeat(0u8).take(size).collect::<Vec<_>>());
+//         });
+//     }
+//     group.finish()
+// }
+
+fn sets_difference(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("sets_difference_close_cardinal_skipset_traverse");
+    for size in [50, 500, 5000, 50000].iter() {
+        if size > &1000 {
+            group.sample_size(30);
+        }
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut sets1 = SkipSet::new();
+            let mut sets2 = SkipSet::new();
+            for i in 0..size {
+                sets1.add(i * 2);
+                sets2.add(i * 3);
+            }
+            b.iter(|| {
+                let _: Vec<_> = sets1.difference_traverse(&sets2).collect();
+            })
+        });
     }
 
-    c.bench_function("skipset close cardinal difference traverse", |b| {
-        b.iter(|| {
-            let _: Vec<_> = sets1.difference_traverse(&sets2).collect();
-        })
-    });
+    group.finish();
 
-    c.bench_function("skipset close cardinal difference search", |b| {
-        b.iter(|| {
-            let _: Vec<_> = sets1.difference_search(&sets2).collect();
-        })
-    });
-}
-
-fn hashset_cardinal_close_benchmark(c: &mut Criterion) {
-    let mut sets1 = HashSet::new();
-    let mut sets2 = HashSet::new();
-    for i in 0..500 {
-        sets1.insert(i * 2);
-        sets2.insert(i * 3);
+    let mut group = c.benchmark_group("sets_difference_close_cardinal_skipset_search");
+    for size in [50, 500, 5000, 50000].iter() {
+        if size > &1000 {
+            group.sample_size(10);
+        }
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut sets1 = SkipSet::new();
+            let mut sets2 = SkipSet::new();
+            for i in 0..size {
+                sets1.add(i * 2);
+                sets2.add(i * 3);
+            }
+            b.iter(|| {
+                let _: Vec<_> = sets1.difference_search(&sets2).collect();
+            })
+        });
     }
 
-    c.bench_function("hashset close cardinal difference", |b| {
-        b.iter(|| {
-            let _: Vec<_> = sets1.difference(&sets2).collect();
-        })
-    });
-}
+    group.finish();
 
-fn btreeset_cardinal_close_benchmark(c: &mut Criterion) {
-    let mut sets1 = BTreeSet::new();
-    let mut sets2 = BTreeSet::new();
-    for i in 0..500 {
-        sets1.insert(i * 2);
-        sets2.insert(i * 3);
+    let mut group = c.benchmark_group("sets_difference_close_cardinal_hashset");
+    for size in [50, 500, 5000, 50000].iter() {
+        if size > &1000 {
+            group.sample_size(10);
+        }
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut sets1 = HashSet::new();
+            let mut sets2 = HashSet::new();
+            for i in 0..size {
+                sets1.insert(i * 2);
+                sets2.insert(i * 3);
+            }
+            b.iter(|| {
+                let _: Vec<_> = sets1.difference(&sets2).collect();
+            })
+        });
     }
 
-    c.bench_function("btreeset close cardinal difference", |b| {
-        b.iter(|| {
-            let _: Vec<_> = sets1.difference(&sets2).collect();
-        })
-    });
+    group.finish();
+
+    let mut group = c.benchmark_group("sets_difference_close_cardinal_btreeset");
+    for size in [50, 500, 5000, 50000].iter() {
+        if size > &1000 {
+            group.sample_size(30);
+        }
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            let mut sets1 = BTreeSet::new();
+            let mut sets2 = BTreeSet::new();
+            for i in 0..size {
+                sets1.insert(i * 2);
+                sets2.insert(i * 3);
+            }
+            b.iter(|| {
+                let _: Vec<_> = sets1.difference(&sets2).collect();
+            })
+        });
+    }
+
+    group.finish();
 }
 
 criterion_group!(
     benches,
-    skipset_cardinal_close_benchmark,
-    hashset_cardinal_close_benchmark,
-    btreeset_cardinal_close_benchmark
+    sets_difference,
 );
 criterion_main!(benches);
